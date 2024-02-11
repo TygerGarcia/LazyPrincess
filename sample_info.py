104f3f2ce35e8a45de64a451807f8ac2018599e7
TygerGarcia <thabetheesihle6@gmail.com>
2024-02-11 19:56:40 +0200

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,23 +1,23 @@
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
-USERBOT_STRING_SESSION = ''
+API_ID = '22626500' 
+API_HASH = 'ff3920ae98ae11d37d0fad07dd965360'
+BOT_TOKEN = '6927318788:AAHgpm8VxegFGECfN2ENXW6ltRZaNEi17UE'
+USERBOT_STRING_SESSION = 'BAFZQMQAq9e7o035lJrU_qghgpqpI5g64BWjS5V9BCjfqI4XJmdaGyUA3Js-ftRIsKkxCCuxlF2yWXjA5VTlquyKTM4z4CYpOqrPi1IThPUFKbjntmh8_HGZm5bhu7wvMFUFG6BTeBUwjjIIxwiowDuyEuKBnnq6inPj-w_30LCPI-hCwtKfu6hXClE1ax-4tABZ-7J2xwu_ryPX7bWhAbWOMyRJlZ-w6CQwO7FgLbZOyrZAXwDvOdjLG5X_D2IiYvDGRH0L09Dj9t_EYwWx4vuk92GFVzhOFbaGFRxjnNEJ_urODzyv0dtAJLRugysP9AgEjFgLICAlbjTX7_yecHiMcLN42AAAAAFMaI7PAA'
 
 # Bot settings
 CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+ADMINS = ['Pop Cash', 'admin123', 98765432]
+CHANNELS = [-'My Ultroid Logs', -100987654321, 'channelusername']
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
+DATABASE_URI = "mmongodb+srv://MamasBoy:hehe123_@cluster0.ofa6drk.mongodb.net/"
 DATABASE_NAME = 'Telegram'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
